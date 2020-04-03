0255a36d5516895157815642474ced7d812d020d
Oskar Gierszewski <gierszon10@gmail.com>
2020-04-03 02:41:07 +0200

    Updates

---
+++ precache-manifest.a37dc17f91d1b3a6c3dcbbb58f65cfdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942fd2cf04ec66c1e030281b99a0eb01",
+    "url": "/react-poker-helper/index.html"
+  },
+  {
+    "revision": "3edb661d4b7365997746",
+    "url": "/react-poker-helper/static/css/main.43a00c1b.chunk.css"
+  },
+  {
+    "revision": "7e0823eaf20209c3430c",
+    "url": "/react-poker-helper/static/js/2.7cba449a.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/react-poker-helper/static/js/2.7cba449a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3edb661d4b7365997746",
+    "url": "/react-poker-helper/static/js/main.17ca4611.chunk.js"
+  },
+  {
+    "revision": "d31f09e1cb210630b50d",
+    "url": "/react-poker-helper/static/js/runtime-main.e89a0857.js"
+  }
+]);
